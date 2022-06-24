f994239063f363504bd51142a845cf633f1ca926
Helori Belbeoch <helori.belbeoch@grdf.fr>
2022-06-24 09:43:45 +0200

    [UPDATE] modify name creator + dell role=""

--- js/controller/home.js
+++ js/controller/home.js
@@ -108,7 +108,7 @@ app.controller("home", ['$scope','$http','$controller', '$compile', "$parse", 'm
                 $scope.idMax = comment.id > $scope.idMax ? comment.id : $scope.idMax ;
                 var authorizationCurrentUser = ($scope.currentUser.username == comment.user.username ? true : false)
                 bodyHTML = bodyHTML + `<div class="mt-3"> 
-                                            <div class="row bg-white rounded-1 p-3" data-ng-class="`+authorizationCurrentUser+` ? 'border-moderate-blue' : ''" id="`+comment.id+`" role="main">
+                                            <div class="row bg-white rounded-1 p-3" data-ng-class="`+authorizationCurrentUser+` ? 'border-moderate-blue' : ''" id="`+comment.id+`">
                                                 <div class="col-sm-8 col-md-10 col-lg-10 col-xl-10 order-sm-1">
                                                     <div class="row d-flex align-items-center">
                                                         <div class="col-2 col-sm-3">
@@ -197,7 +197,7 @@ app.controller("home", ['$scope','$http','$controller', '$compile', "$parse", 'm
                         var model = $parse("date"+underComment.id)
                         model.assign($scope,dataCurrent)
 
-                        bodyHTML = bodyHTML + `<div class="row bg-white rounded-3 p-3 mb-3" id="`+underComment.id+`" data-ng-class="`+authorizationCurrentUser+` ? 'border-moderate-blue' : ''" id="`+comment.id+`" role="article">
+                        bodyHTML = bodyHTML + `<div class="row bg-white rounded-3 p-3 mb-3" id="`+underComment.id+`" data-ng-class="`+authorizationCurrentUser+` ? 'border-moderate-blue' : ''" id="`+comment.id+`">
                                                     <div class="col-sm-8 col-md-9 col-lg-10 col-xl-10 order-sm-1">
                                                         <div class="row d-flex align-items-center"><div class="col-3 col-sm-3 col-md-3">
                                                             <img src="`+underComment.user.image.png+`" class="img-fluid" alt="userProfileUnderComment`+underComment.id+`">
@@ -318,7 +318,7 @@ app.controller("home", ['$scope','$http','$controller', '$compile', "$parse", 'm
                                         Challenge by <a href="https://www.frontendmentor.io/challenges/interactive-comments-section-iG1RugEG9" target="_blank">Frontend Mentor</a>.
                                     </div>
                                     <div class="">
-                                        Coded by <a href="https://github.com/HB-Naoned/interactive-comments-section">GCO44</a>.
+                                        Coded by <a href="https://github.com/HB-Naoned/interactive-comments-section">HB-Naoned</a>.
                                     </div>
                                 </div>
                            </footer>`
